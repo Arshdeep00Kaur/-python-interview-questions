016893b016fe5b30178eebbfe635c520c4301da8
Arshdeep00Kaur <165985272+Arshdeep00Kaur@users.noreply.github.com>
2024-09-06 22:07:29 +0530

    Add files via upload

---
+++ list vs tuple .py
@@ -0,0 +1,14 @@
+"""   Major difference between list and tuple is :
+      list is mutable(changeable)
+      tuple is non mutable(not changeable)"""
+
+# list
+# list=[1,"apple","Arsh"]
+# list[0]="22"
+# print(list)
+
+# tuple
+# tuple1=("apple","banana")
+# tuple1[0]="22"  # this will raise an error
+# print(tuple1)
+

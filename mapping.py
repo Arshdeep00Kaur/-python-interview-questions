471ab7818748ff1d1c357879324c09edc6a375f1
Arshdeep00Kaur <165985272+Arshdeep00Kaur@users.noreply.github.com>
2024-09-07 18:35:52 +0530

    Add files via upload

---
+++ mapping.py
@@ -0,0 +1,22 @@
+""" let us suppose we have to create a function that will return square of no."""
+# def square(a):
+#     return a**2
+# print(square(2))
+
+""" here we have enter values again again i.e we have to call function again and again """
+
+""" we can enter all the values at same time using map function"""
+# def square(a):
+#     return a**2
+
+# a=map(square,(1,2,3,4,5))
+# print(a)
+
+# # map returns list therefore we have call a list
+# print(list(a))
+
+# def concat(a,b):
+#     return a+b
+
+# c=map(concat,("arsh","madhav"),("arun","kashish"))
+# print(list(c))

59308aceb05231bb14bf2de33d50914dcea26f13
Arshdeep00Kaur <165985272+Arshdeep00Kaur@users.noreply.github.com>
2024-09-07 18:35:38 +0530

    Add files via upload

---
+++ practice ques1.py
@@ -0,0 +1,24 @@
+"""  converting list to string"""
+# list1=["apple","mango","banana"]
+# str=" ".join(list1)
+# print(str)
+
+""" converting list to tuple """
+# list1=["apple","mango","banana"]
+# tup=tuple(list1)
+# print(tup)
+
+""" converting list to set """
+list1=["apple","mango","banana"]
+set1=set(list1)
+print(set1)
+
+""" adding one elememt at the end of the list """
+# list1=["apple","mango","banana"]
+# print(list1.append("lichi"))
+# print(list1)
+
+""" adding a another list  at the end of the list """
+list1=["apple","mango","banana"]
+list1.extend(["arsh","arun"])
+print(list1)
